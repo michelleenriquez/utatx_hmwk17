117ca9c6edd44de65ff93375acb92c279bfaf2b1
Daniela Jovanovic <danielajovanovic8@gmail.com>
2020-09-01 22:06:22 -0500

    Added Java for events page

---
+++ JS/Events.js
@@ -0,0 +1,78 @@
+
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+var button = document.getElementsByTagName("button")[0];
+
+var label = document.getElementsByTagName("label")[0];
+
+img.onclick = function() {
+    modal.style.display = "block";}
+
+span.onclick = function() {
+    modal.style.display = "none";}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";}
+
+button.onclick = function() {
+        modal.style.display = "none";}
+
+label.onclick = function() {
+            modal.style.display = "none";}
+
+    var modal = document.getElementById("myModal");
+
+    var img = document.getElementById("myBtn2");
+    
+    var span = document.getElementsByClassName("close")[0];
+    
+    img.onclick = function() {
+        modal.style.display = "block";}
+    
+    span.onclick = function() {
+        modal.style.display = "none";}
+    
+    window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";}
+
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("myBtn3");
+
+var span = document.getElementsByClassName("close")[0];
+
+img.onclick = function() {
+    modal.style.display = "block";}
+
+span.onclick = function() {
+    modal.style.display = "none";}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";}
+
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("myBtn4");
+
+var span = document.getElementsByClassName("close")[0];
+
+img.onclick = function() {
+    modal.style.display = "block";}
+
+span.onclick = function() {
+    modal.style.display = "none";}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";}
+        }
+    }
+}
+}
